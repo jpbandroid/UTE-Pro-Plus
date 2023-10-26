43713d492028ba8dd4882f8796f738430bbc9f64
jpb <jakub.baranski.33@gmail.com>
2023-10-26 21:00:51 +0100

    Add date insertion

---
+++ UltraTextEdit/Views/DateDialog.xaml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace UltraTextEdit.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DateDialog : Page
+    {
+        public DateDialog()
+        {
+            this.InitializeComponent();
+            // Create a ListView for the user to select the date format
+            listView.SelectionMode = ListViewSelectionMode.Single;
+
+            // Create a list of date formats to display in the ListView
+            List<string> dateFormats = new List<string>();
+            dateFormats.Add(DateTime.Now.ToString("dd.M.yyyy"));
+            dateFormats.Add(DateTime.Now.ToString("M.dd.yyyy"));
+            dateFormats.Add(DateTime.Now.ToString("dd MMM yyyy"));
+            dateFormats.Add(DateTime.Now.ToString("dddd, dd MMMM yyyy"));
+            dateFormats.Add(DateTime.Now.ToString("dd MMMM yyyy"));
+            dateFormats.Add(DateTime.Now.ToString("hh:mm:ss tt"));
+            dateFormats.Add(DateTime.Now.ToString("HH:mm:ss"));
+            dateFormats.Add(DateTime.Now.ToString("dddd, dd MMMM yyyy, HH:mm:ss"));
+            dateFormats.Add(DateTime.Now.ToString("dd MMMM yyyy, HH:mm:ss"));
+            dateFormats.Add(DateTime.Now.ToString("MMM dd, yyyy"));
+
+            // Set the ItemsSource of the ListView to the list of date formats
+            listView.ItemsSource = dateFormats;
+        }
+    }
+}
