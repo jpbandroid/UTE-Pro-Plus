335b9ab86c6a7362e804d4ac8d893713b6b52f70
jpb <jakub.baranski.33@gmail.com>
2024-03-26 06:52:58 +0000

    Use CommunityToolkit Settings

--- UltraTextEdit/Views/SettingsPage.xaml.cs
+++ UltraTextEdit/Views/SettingsPage.xaml.cs
@@ -17,4 +17,38 @@ public sealed partial class SettingsPage : Page
         ViewModel = App.GetService<SettingsViewModel>();
         InitializeComponent();
     }
+
+    private void GH_Navigate(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        // The URI to launch
+        string uriToLaunch = @"https://github.com/jpbandroid/UTE-Pro-Plus";
+
+        // Create a Uri object from a URI string 
+        var uri = new Uri(uriToLaunch);
+
+        // Launch the URI
+        async void DefaultLaunch()
+        {
+            // Launch the URI
+            var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+
+            if (success)
+            {
+                // URI launched
+            }
+            else
+            {
+                // URI launch failed
+            }
+        }
+        DefaultLaunch();
+    }
+
+    private void BackButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        if (this.Frame.CanGoBack)
+        {
+            this.Frame.GoBack();
+        }
+    }
 }
